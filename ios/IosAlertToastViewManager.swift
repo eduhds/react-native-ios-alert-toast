c70f7412260a9add4c45ddb424f41614ed701f5e
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-07-10 10:53:00 -0300

    fix: alert toast nil options

--- ios/IosAlertToastViewManager.swift
+++ ios/IosAlertToastViewManager.swift
@@ -5,11 +5,11 @@ struct AlertToastContentView: View {
     private var mode: AlertToast.DisplayMode = .alert
     private var type: AlertToast.AlertType = .regular
     private var title: String = ""
-    private var subTitle: String = ""
+    private var subTitle: String? = nil
     
     @State private var showingAlert = false
     
-    init(mode: AlertToast.DisplayMode, type: AlertToast.AlertType, title: String, subTitle: String) {
+    init(mode: AlertToast.DisplayMode, type: AlertToast.AlertType, title: String, subTitle: String?) {
         self.mode = mode
         self.type = type
         self.title = title
@@ -37,52 +37,50 @@ struct AlertToastContentView: View {
 
 @objc(IosAlertToastViewManager)
 class IosAlertToastViewManager: RCTViewManager {
-
-  override func view() -> (IosAlertToastView) {
-    return IosAlertToastView()
-  }
-
-  @objc override static func requiresMainQueueSetup() -> Bool {
-    return false
-  }
+    override func view() -> (IosAlertToastView) {
+        return IosAlertToastView()
+    }
+    
+    @objc override static func requiresMainQueueSetup() -> Bool {
+        return false
+    }
 }
 
 class IosAlertToastView : UIView {
-
     @objc var toast: [String: Any] = [:] {
         didSet {
             setupView(toast)
         }
     }
-
+    
     func setupView(_ data: [String: Any]) {
         self.backgroundColor = .clear
         
-        let toastTitle = data["title"] as! String
+        let toastTitle: String = data["title"] as! String
         
-        let toastSubtitle = data["subTitle"] as! String
+        let toastSubtitle: String? = data["subTitle"] as? String
         
-        let toastMode: AlertToast.DisplayMode = switch data["mode"] as! String {
-            case "alert": .alert
-            case "banner-pop": .banner(.pop)
-            case "banner-slide": .banner(.slide)
-            case "hud": .hud
-            default: .alert
+        let toastMode: AlertToast.DisplayMode = switch data["mode"] as? String {
+        case "alert": .alert
+        case "banner-pop": .banner(.pop)
+        case "banner-slide": .banner(.slide)
+        case "hud": .hud
+        default: .alert
         }
         
-        let toastType: AlertToast.AlertType = switch data["type"] as! String {
-            case "regular": .regular
-            case "error": .error(Color.red)
-            case "complete": .complete(Color.green)
-            case "loading": .loading
-            default: .regular
+        let toastType: AlertToast.AlertType = switch data["type"] as? String {
+        case "regular": .regular
+        case "error": .error(Color.red)
+        case "complete": .complete(Color.green)
+        case "loading": .loading
+        default: .regular
         }
         
         let contentView = AlertToastContentView(mode: toastMode, type: toastType, title: toastTitle, subTitle: toastSubtitle)
-
+        
         let alertToastView: UIView = UIHostingController(rootView: contentView).view
         addSubview(alertToastView)
-
+        
         alertToastView.backgroundColor = .clear
         alertToastView.translatesAutoresizingMaskIntoConstraints = false
         
